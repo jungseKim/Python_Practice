5b2f96215b426a6aba1135104a73b24962dd0199
bible6146 <bible6146@naver.com>
2021-09-02 16:45:07 +0900

    ddd

--- Test1.py
+++ Test1.py
@@ -15,8 +15,19 @@
 # print(int(A/B))
 # print(A%B)
 
-A,B,C=map(int,input().split())
-print((A+B)%C)
-print(((A%C) + (B%C))%C)
-print((A*B)%C)
-print(((A%C) * (B%C))%C)
+# A,B,C=map(int,input().split())
+# print((A+B)%C)
+# print(((A%C) + (B%C))%C)
+# print((A*B)%C)
+# print(((A%C) * (B%C))%C)
+
+num=int(input())
+for i in range(num):
+    strs=''
+    if i<num/2:
+        for j in range(i+1):
+            strs+=str(j)
+    else :
+        for j in range(num-i):
+            strs+=str(j)
+    print(strs)
--- hellow.py
+++ hellow.py
@@ -1,6 +1,42 @@
 # num,num2=input().split()
 # print(int(num)+int(num2))
 
-num,num2=map(int,input().split())
-print(num+num2)
+# num,num2=map(int,input().split())
+# print(num+num2)
 
+# list=[]
+# list2=[1,2,3,True,False]
+# list3=[(1,2,3),{'name':'user'},[1,2,3]]
+# list4=[list,list2]
+# print(list,list2)
+# print(list3)
+# print(len(list4),list4)
+
+# list=[1,2,3,4,5]
+# print(list[2],list[3])
+
+# list=[0,1,2,3,4,5,6,7,8]
+# print(list.index(len(list)-1))##이것만반환됨
+# list.insert(3,-1)
+# print(list)
+# list.append(11)
+# print(list)
+# list.sort()
+# print(list)
+# list.sort(reverse=True)
+# print(list)
+
+# list=[0,1,2,3,4,5]
+# list.insert(0,6)
+# list.append(0)
+# print(list)
+# list.sort()
+# print(list)
+
+# list=[1,2,3,4,5]
+# # list.remove(int(input()))
+# a=str(list)
+# print(type(a[0]))
+# print(a[0])
+x,y,z=10,40,30
+print(x,y)
