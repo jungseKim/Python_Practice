9aa237d2791e7e035b2c674779fa11728bbafcb0
jungsekim <https://github.com/jungseKim/AndroidSharedCarAppDemo.git>
2021-09-17 15:07:47 +0900

    practice

---
+++ Coding_test/Alone_test/IDrecommendation.py
@@ -0,0 +1,21 @@
+def solution(new_id):
+    new_id = new_id.lower()
+    arr = ['.', '-', '_']
+#      소문자, 숫자, 빼기(-), 밑줄(_), 마침표(.)
+    for x in new_id:
+        if(arr.count(x) == 0 and not(96 < ord(x) < 123)):
+            new_id = new_id.replace(x, '')
+    new_id = new_id.replace('..', '.')
+    if(new_id[0] == '.'):
+        del new_id[0]
+    if(new_id[-1] == '.'):
+        del new_id[-1]
+    if(len(new_id) == 0):
+        new_id = 'a'
+    if(len(new_id) > 15):
+        del new_id[15, -1]
+    while len(new_id) > 2:
+        new_id = new_id+new_id[-1]
+
+    answer = ''
+    return answer
