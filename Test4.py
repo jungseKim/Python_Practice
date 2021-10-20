35af1b83bd9f10ec049a80260483afd46cb54765
jungsekim <bible6146@naver.com>
2021-10-20 20:48:32 +0900

    dd

--- Test4.py
+++ Test4.py
@@ -71,20 +71,142 @@
 # sumj(*a)
 
 
-a = [1, 3, 6, 9, 12, 15, 18, 21]
-b = 8
+# a = [1, 3, 6, 9, 12, 15, 18, 21]
+# b = 8
 
 
-def binary(v, a, low, high):
-    mid = (low+high)//2
-    if(low > high):
-        return "false"
-    if(a[mid] == v):
-        return mid
-    if(a[mid] > v):
-        return binary(v, a[:mid], low, mid-1)
+# def binary(v, a, low, high):
+#     mid = (low+high)//2
+#     if(low > high):
+#         return "false"
+#     if(a[mid] == v):
+#         return mid
+#     if(a[mid] > v):
+#         return binary(v, a[:mid], low, mid-1)
+#     else:
+#         return binary(v, a[mid:], mid+1, high)
+#  prin
+
+# print(binary(b, a, 0, len(a)))
+
+# a = list(map(int, input().split()))
+# a.insert(0, 3)
+# del a[:]
+# # print(a)
+# a, b = map(int, input().split())
+# print('ddd') if a+b > 0 else print('np')
+
+
+# def print2(list):
+#     list = [x for x in list if x % 2 == 0]
+#     for x in list:
+#         print(x)
+
+
+# print2([1, 2, 3, 4, 5, 6, 7, 8, 9])
+# list = [x*3 for x in range(1, 10)]
+# print(list)
+
+# def min_int(*dd):
+#     return min(dd)
+
+
+# print(min_int(12323, 32323, 3232, 3, 2, 32, 3, 23, 2, 32, 323223323))
+
+
+def fa(x):
+    temp = x
+    x = x-1
+    while x > 0:
+        temp = temp*x
+        x = x-1
+    return temp
+
+
+# print(fa(5))
+
+
+def fa2(x):
+    if(x == 1):
+        return 1
     else:
-        return binary(v, a[mid:], mid+1, high)
+        return fa2(x-1)*x
 
 
-print(binary(b, a, 0, len(a)))
+# print(fa2(5))
+
+# print(type(i for i in range(3)))
+
+# def my(*list):
+#     if len(list)==3:
+#         return (x for x in range(list[0],list[1],list[2]))
+
+# a = [1, 2]
+
+
+# def ar(v, list, low, high):
+#     mid = (low+high)//2
+#     if(low > high):
+#         return "false"
+#     if(list[mid] == v):
+#         return "True"
+#     elif(list[mid] < v):
+#         return ar(v, list, mid+1, high)
+#     else:
+#         return ar(v, list, low, mid-1)
+
+
+# print(ar(10, [1, 2, 3, 4, 5], 0, 4))
+
+
+# def merge(a, b):
+#     temp = []
+#     while True:
+#         if(len(a) == 0 or len(b) == 0):
+#             temp.extend(a)
+#             temp.extend(b)
+#             return temp
+#         if(a[0] > b[0]):
+#             temp.append(b.pop(0))
+#         else:
+#             temp.append(a.pop(0))
+
+
+# def merge_sort(list):
+#     check = len(list)
+#     if(check == 1):
+#         return list
+#     else:
+#         a = merge_sort(list[:check//2])
+#         b = merge_sort(list[check//2:])
+#         return merge(b, a)
+
+
+# print(merge_sort([1, 3, 478, 5643, 423]))
+# add()
+# a = 1
+# temp = 0
+# while a != 0:
+#     a = int(input())
+#     temp += a
+# print(temp)
+
+
+# def fa():
+#     return add
+
+
+# print(fa()(1, 2))
+
+
+a = int(input())
+set = [int(input()) for x in range(a)]
+temp = [0]*max(set)
+temp[0] = [1, 0]
+temp[1] = [0, 1]
+for x in range(2, a):
+    a = temp[x-1][0]+temp[x-2][0]
+    b = temp[x-1][1]+temp[x-2][1]
+    temp[x] = [a, b]
+for x in set:
+    print(temp[x][0], temp[x][1])
