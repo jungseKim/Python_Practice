7533083d97372a46b31f018cc91f1197972023e9
bible6146 <bible6146@naver.com>
2021-11-09 12:35:22 +0900

    Q-tableMake

--- python-ai/gym_Test/TicTacToeDemo.py
+++ python-ai/gym_Test/TicTacToeDemo.py
@@ -66,32 +66,78 @@ class Player:
         idx = np.random.choice(len(positions))
         return positions[idx]
 
+class Qplayer:
+    def __init__(self, symbol,gama=0.9,sotf=0.7):
+        self.symbol = symbol
+        self.count = 0
+        self.gama=gama
+        self.soft=sotf
+        self.states=[]
+        self.states_value={}
+        self.exp_rate=0.15
+
+    def update(self,postion):
+        self.states.append(postion)
+        
+    def rewardUpdate(self,reward):
+        for s in reversed(self.states):
+            if self.states_value.get(s) is None:
+                self.states_value[s]=0
+            self.states_value[s]=(1-self.soft)*self.states_value[s]\
+                +self.soft*(self.gama*reward)
+            reward=self.states_value[s]
+        self.states=[]
+
+    def randomChoice(self,positions):
+        idx = np.random.choice(len(positions))
+        return positions[idx]
+
+    def chooseAction(self, positions, current_board):
+        if np.random.uniform(0, 1) <= self.exp_rate:
+            return self.randomChoice(positions)
+        else:
+            value_max = -1
+            for p in positions:
+                next_board = current_board.copy()
+                next_board[p] = self.symbol
+                next_boardHash = str(current_board.reshape(9))
+                value = 0 if self.states_value.get(
+                    next_boardHash) is None else self.states_value.get(next_boardHash)
+                # print("value", value)
+                if value >= value_max:
+                    value_max = value
+                    action = p
+            if value==0:
+                self.randomChoice(positions)
+            return action
 
 if __name__ == "__main__":
-    p1 = Player(1)
+    p1 = Qplayer(1)
     p2 = Player(-1)
     border = Border()
 
-    for x in range(4):
+    for x in range(2000):
 
         while border.done == False:
-           #      print(len(border.availablePositions()))
             if len(border.availablePositions()) == 0:
+                p1.rewardUpdate(0.2)
                 break
             position = border.availablePositions()
             if(border.symbol == 1):
-                action = p1.action(position)
-                border.step(action, 1)
-                # //업대이트
+                action = p1.chooseAction(position,border.state)
+                position=border.step(action, 1)
+                p1.update(position)
             else:
                 action = p2.action(position)
                 border.step(action, -1)
             if(border.done):
                 if border.symbol == 1:
-                    p1.count += 1
+                    p1.rewardUpdate(1)
+                    p1.count+=1
                 #       reward 주고 player 계산
                 else:
+                    p1.rewardUpdate(-1)
                     p2.count += 1
         border.reset()
 
-    print(p1.count, p2.count)
+    print(p1.count/2000)
