843e7b993a98f1226ec81dfadc242a3a74984966
jungsekim <bible6146@naver.com>
2021-11-09 20:04:51 +0900

    complate

--- python-ai/gym_Test/TicTacToeDemo.py
+++ python-ai/gym_Test/TicTacToeDemo.py
@@ -1,7 +1,6 @@
 from pickle import STOP
 import numpy as np
 
-# print(np.zeros((3, 3)))
 
 
 class Border:
@@ -22,13 +21,11 @@ class Border:
         for i in range(self.row):
             for j in range(self.col):
                 if self.state[i, j] == 0:
-                    positions.append((i, j))  # need to be tuple
+                    positions.append((i, j))  
         return positions
 
     def getHash(self):
         return str(self.state.reshape(self.row * self.col))
-        # [0. 0. 0. 0. 0. 0. 1. 0. 0.]
-       #  return self.boardHash
 
     def soyobuCheck(self, endResult):
         for i in range(self.row):
@@ -46,7 +43,6 @@ class Border:
             self.done = True
 
     def step(self, action, symbol):
-        # print(action)
         self.state[action] = symbol
         endResult = symbol*3
 
@@ -105,13 +101,15 @@ class Qplayer:
             for p in positions:
                 next_board = current_board.copy()
                 next_board[p] = self.symbol
-                next_boardHash = str(current_board.reshape(9))
+                next_boardHash = str(next_board.reshape(9))
+    
                 value = 0 if self.states_value.get(
                     next_boardHash) is None else self.states_value.get(next_boardHash)
-                # print("value", value)
+               
                 if value >= value_max:
                     value_max = value
                     action = p
+
             return action
 
 
@@ -121,7 +119,7 @@ if __name__ == "__main__":
     border = Border()
 
     musyoubu = 0
-    for x in range(5000):
+    for x in range(50000):
 
         while border.done == False:
             if len(border.availablePositions()) == 0:
@@ -143,41 +141,42 @@ if __name__ == "__main__":
                     p1.rewardUpdate(1)
                     p1.count += 1
                     p2.rewardUpdate(-1)
-                #       reward 주고 player 계산
                 else:
                     p1.rewardUpdate(-1)
                     p2.rewardUpdate(1)
                     p2.count += 1
-        p2.exp_rate = (0.5-x/500)
-        p1.exp_rate = (0.5-x/500)
-
+        p2.exp_rate = (0.5-x/35000)
+        p1.exp_rate = (0.5-x/35000)
         border.reset()
-
-    while border.done == False:
-        print(border.state)
-        if len(border.availablePositions()) == 0:
-            print('비김')
-            break
-
-        position = border.availablePositions()
-        if(border.symbol == 1):
-            check = int(input("Input your action row:"))
-            if(check < 4):
-                action = (0, check-1)
-            elif(check < 7 and check > 3):
-                action = (1, check-4)
-            else:
-                action = (2, check-7)
-            border.step(action, 1)
-
-        else:
-            action = p2.chooseAction(position, border.state)
-            position = border.step(action, -1)
-
-        if(border.done):
-            if border.symbol == 1:
-
-                print('니가이김')
-            else:
-                print('기계가이김')
+    print(musyoubu/10000, '무승부수')
+    print('게이시작 아무 버튼 눌러주세요')
+    while input() != 's':
+        while border.done == False:
             print(border.state)
+            if len(border.availablePositions()) == 0:
+                print('비김')
+                break
+
+            position = border.availablePositions()
+            if(border.symbol == 1):
+                check = int(input("Input your action row:"))
+                if(check < 4):
+                    action = (0, check-1)
+                elif(check < 7 and check > 3):
+                    action = (1, check-4)
+                else:
+                    action = (2, check-7)
+                border.step(action, 1)
+
+            else:
+                action = p2.chooseAction(position, border.state)
+                position = border.step(action, -1)
+
+            if(border.done):
+                if border.symbol == 1:
+                    print('니가이김')
+                else:
+                    print('기계가이김')
+                print(border.state)
+        print('끝내려면 s 눌러')
+        border.reset()
