d4a2892cc07626ad6249745618e86eb63d879a69
jungsekim <bible6146@naver.com>
2021-11-11 13:50:19 +0900

    dd

--- python-ai/Q-network.py
+++ python-ai/Q-network.py
@@ -1,22 +1,60 @@
 import gym
 import numpy as np
-from numpy.core.fromnumeric import shape
 import tensorflow as tf
-import matplotlib.pyplot as plt
+env = gym.make('FrozenLake-v1')
 
-evn = gym.make('CartPole-v0')
-evn.reset()
-random_episodes = 0
-reward_sum = 0
+input_size = env.observation_space.n
+output_size = env.action_space.n
+ler_late = 0.1
 
-while random_episodes < 10:
-    evn.render()
-    action = evn.action_space.sample()
-    observation, reward, done, _ = evn.step(action)
-    print(observation, reward, done)
-    reward_sum += reward
-    if done:
-        random_episodes += 1
-        print('your episodes', reward_sum)
-        reward_sum = 0
-        evn.reset()
+X = tf.concat([1, input_size])
+W = tf.Variable(tf.random_uniform([input_size, output_size], 0, 0.01))
+
+Qpred = tf.matmul(X, W)
+Y = tf.concat([1, output_size])
+
+loss = tf.reduce_sum(tf.square(Y-Qpred))
+
+train = tf.train.GradientDescentOptimizer(
+    learning_rate=ler_late).minimize(loss)
+
+dis = 0.99
+episodes = 10
+
+rList = []
+
+
+with tf.Session() as sess:
+    init = tf.initialize_all_variables()
+    sess.run(init)
+    for i in range(episodes):
+
+        s = env.reset()
+        e = 1.0/((i/50)+10)
+        rAll = 0
+        done = False
+        local_loss = []
+
+        while not done:
+            Qs = sess.run(Qpred, feed_dict={X: np.one_hot(s)})
+            #  예측값   현재상태 16(스테이트) 4(액션)
+            if np.random.rand(1) < e:
+                a = env.action_space.sample()
+            else:
+                a = np.argmax(Qs)
+
+            s1, reward, done, _ = env.step(a)
+            if done:
+                Qs[0:a] = reward
+            else:
+                Qs1 = sess.run(Qpred, feed_dict={X: np.one_hot(s1)})
+
+                Qs[0, a] = reward+dis*np.max(Qs1)
+
+            sess.run(train, feed_dict={X: np.one_hot(s), Y: Qs})
+
+            rAll += reward
+            s = s1
+        rList.append(rAll)
+
+print(sum(rList)/episodes)
